673b54ce3d61b9abeca125759729f97a9330f277
Chandrakethan <90309231+chandrakethan27@users.noreply.github.com>
2024-02-15 12:14:11 +0530

    starting code

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import {useEffect, useState} from 'react'
+
+const App = () => {
+  const [theme, setTheme] = useState("light")
+  let handleTheme = ()=>{
+    setTheme(theme === 'dark' ? "light":"dark")
+  }
+  
+  useEffect(()=>{
+    if (theme === "dark"){
+      document.documentElement.classList.add("dark");
+    }else{
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme])
+  return (
+    <div className='bg-blue-300 dark:bg-slate-900 w-full h-screen flex justify-center items-center'> 
+      <button onClick={handleTheme} className=' border bg-yellow-200 px-4 py-2 rounded-lg'>
+      Switch
+      </button>
+    </div>
+  )
+}
+export default App
